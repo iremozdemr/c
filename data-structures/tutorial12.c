6f3a3e03be48a0579add76ab52c85d01fac35bdb
İrem Özdemir <irem.ozdemir@etu.edu.tr>
2024-12-03 16:22:14 +0300

    binary search tree

---
+++ data-structures/tutorial12.c
@@ -0,0 +1,91 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node{
+    int data;
+    struct node* left;
+    struct node* right;
+}
+
+typedef struct node node;
+
+node* createNode(int value){
+    node* newnode = (node*) malloc(sizeof(node));
+
+    if(newnode == NULL){
+        return NULL;
+    }
+
+    newnode -> data = value;
+    newnode -> left = NULL;
+    newnode -> right = NULL;
+
+    return newnode;
+}
+
+void insert(node* root,int value){
+    if(root == NULL){
+        createNode(value);
+    }
+    else if(value <= root -> data){
+        root -> left = insert(root -> left,value);
+    }
+    else{
+        root -> right = insert(root -> right,value);
+    }
+}
+
+void delete(node* root,int value){
+    if(root == NULL){
+    }
+    else if(value <= root -> data){
+        root -> left = delete(root -> left,value);
+    }
+    else{
+        root -> right = delete(root -> right,value);
+    }
+}
+
+node* search(node* root, int value){
+    if(root == NULL){
+        return root;
+    }
+    else if(value == root -> data){
+        return root;
+    }
+    else if(value < root -> data){
+        root = root -> left;
+        search(root,value);
+    }
+    else{
+        root = root -> right;
+        search(root,value);
+    }
+}
+
+//left root right
+void inorderTraversal(node* root) {
+    if (root != NULL) {
+        inorderTraversal(root->left);
+        printf("%d ", root->data);
+        inorderTraversal(root->right);
+    }
+}
+
+//root left right
+void preorderTraversal(node* root) {
+    if (root != NULL) {
+        printf("%d ", root->data);
+        preorderTraversal(root->left);
+        preorderTraversal(root->right);
+    }
+}
+
+//left right root
+void postorderTraversal(node* root) {
+    if (root != NULL) {
+        postorderTraversal(root->left);
+        postorderTraversal(root->right);
+        printf("%d ", root->data);
+    }
+}
